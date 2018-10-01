27b4dce36aa8934666f11f665e1eca9ab067d2de
HanSol <hnslzy@gmail.com>
2018-10-02 04:24:47 +0900

    1. 메인 일반회원 로그인 전, 후 / 회원 로그인 선택 / 일반 회원 로그인 / 병원가기

---
+++ admin/js/script2.js
@@ -0,0 +1,22 @@
+( function( $ ) {
+$( document ).ready(function() {
+$('#side_menu > ul > li > a').click(function() {
+  $('#side_menu li').removeClass('active');
+  $(this).closest('li').addClass('active');
+  var checkElement = $(this).next();
+  if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+    $(this).closest('li').removeClass('active');
+    checkElement.slideUp('normal');
+  }
+  if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+    $('#side_menu ul ul:visible').slideUp('normal');
+    checkElement.slideDown('normal');
+  }
+  if($(this).closest('li').find('ul').children().length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+});
+});
+} )( jQuery );
